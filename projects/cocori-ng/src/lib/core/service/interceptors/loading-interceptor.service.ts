b3e20691a976b922f39a80b36265c6ff3b042752
nicos <nico.sesma@gmail.com>
2021-01-16 17:59:29 +0100

    clear loading buttons

--- projects/cocori-ng/src/lib/core/service/interceptors/loading-interceptor.service.ts
+++ projects/cocori-ng/src/lib/core/service/interceptors/loading-interceptor.service.ts
@@ -6,31 +6,35 @@ import { finalize } from 'rxjs/internal/operators';
 import { LoadingService } from '../loading.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class LoadingInterceptorService {
+  activeRequests: number = 0;
 
-    activeRequests: number = 0;
+  constructor(private loadingScreenService: LoadingService) {}
 
-    constructor(
-        private loadingScreenService: LoadingService
-    ) { }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (this.activeRequests === 0) {
+      this.loadingScreenService.startLoading();
+    }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.activeRequests++;
 
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.activeRequests--;
         if (this.activeRequests === 0) {
-            this.loadingScreenService.startLoading();
+          this.loadingScreenService.stopLoading();
         }
+      })
+    );
+  }
 
-        this.activeRequests++;
-
-        return next.handle(request).pipe(
-            finalize(() => {
-                this.activeRequests--;
-                if (this.activeRequests === 0) {
-                    this.loadingScreenService.stopLoading();
-                }
-            })
-        )
-    };
+  forceFinalize() {
+    this.activeRequests = 0;
+    this.loadingScreenService.stopLoading();
+  }
 }
