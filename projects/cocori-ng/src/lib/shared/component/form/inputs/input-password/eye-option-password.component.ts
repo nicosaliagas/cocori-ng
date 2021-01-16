8e138bc0de3ee8b67e85bb7015441f47bb8eb65b
nicos <nico.sesma@gmail.com>
2021-01-16 21:39:26 +0100

    reveal password on click

--- projects/cocori-ng/src/lib/shared/component/form/inputs/input-password/eye-option-password.component.ts
+++ projects/cocori-ng/src/lib/shared/component/form/inputs/input-password/eye-option-password.component.ts
@@ -1,46 +1,52 @@
 import { Component, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Component({
-    selector: 'eye-option-password',
-    template: `
-        <i class="eyeIcon material-icons">
-            <ng-container *ngIf="!revealPasswordStatus" >visibility</ng-container>
-            <ng-container *ngIf="revealPasswordStatus" >visibility_off</ng-container>
-        </i>
+  selector: "eye-option-password",
+  template: `
+    <i class="eyeIcon material-icons">
+      <ng-container *ngIf="!revealPasswordStatus">visibility</ng-container>
+      <ng-container *ngIf="revealPasswordStatus">visibility_off</ng-container>
+    </i>
+  `,
+  styles: [
+    `
+      .eyeIcon {
+        right: 8px;
+        font-size: 20px;
+        cursor: pointer;
+        position: absolute;
+        transform: translateY(-25%);
+      }
     `,
-    styles: [`
-        .eyeIcon {
-            right: 8px;
-            font-size: 20px;
-            cursor: pointer;
-            position: absolute;
-            transform: translateY(-25%);
-        }
-    `]
+  ],
 })
 export class EyeOptionPasswordComponent {
-    @Output() revealPassword: EventEmitter<boolean> = new EventEmitter();
-    revealPasswordStatus: boolean = false;
-    private last: MouseEvent;
+  @Output() revealPassword: EventEmitter<boolean> = new EventEmitter();
+  revealPasswordStatus: boolean = false;
+  private last: MouseEvent;
 
-    @HostListener('mousedown', ['$event']) eventMouseDown(event: any) {
-        this.revealPassword.emit(true);
-        this.revealPasswordStatus = true;
+  @HostListener("click", ["$event"]) eventMouseDown(event: any) {
+    if (this.revealPasswordStatus) {
+      this.hidePassword();
+    } else {
+      this.revealPassword.emit(true);
     }
 
-    @HostListener('mouseup', ['$event']) eventMouseUp(event: any) {
-        this.hidePassword();
-    }
+    this.revealPasswordStatus = !this.revealPasswordStatus;
+  }
 
-    @HostListener('document:mousemove', ['$event']) eventMouseMove(e: MouseEvent) {
-        if (this.last && e.clientX !== this.last.clientX) {
-            this.hidePassword();
-        }
-        this.last = e;
-    }
+  // @HostListener('mouseup', ['$event']) eventMouseUp(event: any) {
+  //     this.hidePassword();
+  // }
 
-    hidePassword() {
-        this.revealPassword.emit(false);
-        this.revealPasswordStatus = false;
-    }
+  // @HostListener('document:mousemove', ['$event']) eventMouseMove(e: MouseEvent) {
+  //     if (this.last && e.clientX !== this.last.clientX) {
+  //         this.hidePassword();
+  //     }
+  //     this.last = e;
+  // }
+
+  hidePassword() {
+    this.revealPassword.emit(false);
+  }
 }
