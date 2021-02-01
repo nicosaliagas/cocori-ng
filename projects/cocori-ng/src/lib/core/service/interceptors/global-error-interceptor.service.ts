aa5b4d98921ce826ac808bed7d55df7bad8801d8
nicos <nico.sesma@gmail.com>
2021-02-01 14:25:46 +0100

    gestion des erreurs, déploiement à chaud : comptage des rechargement de page et limite à 5 ttes les 60 secondes

--- projects/cocori-ng/src/lib/core/service/interceptors/global-error-interceptor.service.ts
+++ projects/cocori-ng/src/lib/core/service/interceptors/global-error-interceptor.service.ts
@@ -1,30 +1,58 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { StorageService } from '../storage.service';
+
+const LIMIT_MAX_RELOAD: number = 5
+const TIME_MAX_RELOAD: number = 60 // secondes
+
+export interface ReloadPageModel {
+  date: Date,
+  count: number,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalErrorInterceptorService implements ErrorHandler {
-
   // public globalErrorsHandlerSubject: Subject<boolean> = new Subject<boolean>();
-  
-  constructor(private injector: Injector) { }
+
+  constructor(
+    private injector: Injector,
+    private storageService: StorageService) { }
 
   handleError(error: any): void {
     const chunkFailedMessage = /Loading chunk [\d]+ failed/;
 
     const router = this.injector.get(Router);
 
-    console.error("handleError errors:");
-    
-    console.error(error.stack.toString());
+    if (error.stack) {
+      console.error("details errors 😱 :");
+      console.error(error.stack.toString());
+    } else {
+      console.error("error undefined 😡");
+    }
 
     if (chunkFailedMessage.test(error.message)) {
-      console.log("Page reloaded cause : errors chunk versions")
-      
       // this.globalErrorsHandlerSubject.next(true)
-
-      window.location.reload();
+      this.handleReloadPage()
     }
+  }
+
+  private handleReloadPage() {
+    console.log('%cPage reloaded : errors chunk versions', 'color: blue;')
+
+    const reloadPage: ReloadPageModel = this.storageService.getSessionStorageItem('reloadPage');
+
+    if (!reloadPage) {
+      this.storageService.setSessionStorageItem('reloadPage', <ReloadPageModel>{ date: new Date(), count: 1 })
+    } else if (reloadPage.count <= LIMIT_MAX_RELOAD && (new Date().getTime() - new Date(reloadPage.date).getTime()) / 1000 < TIME_MAX_RELOAD) {
+      this.storageService.setSessionStorageItem('reloadPage', <ReloadPageModel>{ date: reloadPage.date, count: reloadPage.count + 1 })
+    } else {
+      this.storageService.deleteSessionStorageItem('reloadPage')
+      return;
+    }
+
+    window.location.reload();
   }
 }
