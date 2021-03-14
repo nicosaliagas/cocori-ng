bf7f3862b9a9564a4fd54d3c0c3594e8868ef0c2
nicos <nico.sesma@gmail.com>
2021-03-14 08:47:04 +0100

    tests unitaires sur la modal filtres

--- projects/cocori-ng/src/lib/shared/component/datagrid/cocoring-datagrid-filter-modal/cocoring-datagrid-filter-column/cocoring-datagrid-filter-column.component.spec.ts
+++ projects/cocori-ng/src/lib/shared/component/datagrid/cocoring-datagrid-filter-modal/cocoring-datagrid-filter-column/cocoring-datagrid-filter-column.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ColumnDatagridModel } from 'projects/cocori-ng/src/lib/core/model/component-datagrid.model';
 import { DatagridService } from 'projects/cocori-ng/src/lib/core/service/datagrid/datagrid.service';
 
@@ -32,18 +34,81 @@ describe('CocoringDatagridFilterColumnComponent', () => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     formBuilderSpy = jasmine.createSpyObj('FormBuilder', ['group']);
 
-    // datagridService = TestBed.inject(DatagridService);
-
     datagridService = new DatagridService(httpClientSpy as any, formBuilderSpy as any)
-    expectedColumn = <ColumnDatagridModel>{ dataField: 'mockDatafield', caption: 'Test Caption', visible: true, sort: 'NONE' };
 
-    component.column = expectedColumn
     component.datagridService = datagridService
-
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the name of the column', () => {
+    const currentColumn: ColumnDatagridModel = {
+      caption: 'Test Caption',
+      dataField: 'testdataField',
+      visible: true,
+      sort: 'NONE',
+      dataType: 'string',
+    }
+
+    component.column = currentColumn
+
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('.column-caption'));
+
+    expect(element.nativeElement.textContent).toEqual('Test Caption');
+    expect(element.nativeElement).not.toHaveClass('column-no-visible')
+  });
+
+  it('should check if the column is marked as not visible', () => {
+    const currentColumn: ColumnDatagridModel = {
+      caption: 'Test Caption',
+      dataField: 'testdataField',
+      visible: false,
+      sort: 'NONE',
+      dataType: 'string',
+    }
+
+    component.column = currentColumn
+
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('.column-caption'));
+
+    expect(element.nativeElement).toHaveClass('column-no-visible')
+  });
+
+  it('should toggle a column visibility', () => {
+    const currentColumn: ColumnDatagridModel = {
+      caption: 'Test Caption',
+      dataField: 'testdataField',
+      visible: true,
+      sort: 'NONE',
+      dataType: 'string',
+    }
+
+    component.column = currentColumn
+
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('.column-caption'));
+
+    expect(element.nativeElement).not.toHaveClass('column-no-visible')
+
+    const button: DebugElement = fixture.debugElement.query(By.css('.button-visibility'));
+
+    button.triggerEventHandler('click', null);
+
+    expect(component.column.visible).toEqual(false)
+
+    expect(element.nativeElement).toHaveClass('column-no-visible')
+
+    button.triggerEventHandler('click', null);
+
+    expect(component.column.visible).toEqual(true)
+
+    expect(element.nativeElement).not.toHaveClass('column-no-visible')
   });
 });
--- projects/cocori-ng/src/lib/shared/component/datagrid/cocoring-datagrid-filter-modal/cocoring-datagrid-filter-modal.component.spec.ts
+++ projects/cocori-ng/src/lib/shared/component/datagrid/cocoring-datagrid-filter-modal/cocoring-datagrid-filter-modal.component.spec.ts
@@ -1,8 +1,10 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
 
-import { ConfigDatagridModel } from '../../../../core/model/component-datagrid.model';
+import { ColumnDatagridModel, ConfigDatagridModel } from '../../../../core/model/component-datagrid.model';
 import { DatagridService } from '../../../../core/service/datagrid/datagrid.service';
 import { CocoringDatagridFilterModalComponent } from './cocoring-datagrid-filter-modal.component';
 
@@ -16,8 +18,8 @@ describe('CocoringDatagridFilterModalComponent', () => {
   let config: ConfigDatagridModel = {
     withBatchProcessing: true,
     columns: [
-      { dataField: 'testDatafield', dataType:'string', caption: 'Test Caption', visible: true }, 
-      { dataField: 'testDatafield2', dataType:'string', caption: 'Test Caption 2', visible: true }]
+      { dataField: 'testDatafield', dataType: 'string', caption: 'Test Caption', visible: true },
+      { dataField: 'testDatafield2', dataType: 'string', caption: 'Test Caption 2', visible: true }]
   }
 
   beforeEach(async () => {
@@ -57,10 +59,85 @@ describe('CocoringDatagridFilterModalComponent', () => {
 
     datagridService = new DatagridService(httpClientSpy as any, formBuilderSpy as any)
 
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display name a selected column in the header of the modal', () => {
+    const currentColumn: ColumnDatagridModel = {
+      caption: 'Test Caption',
+      dataField: 'testdataField',
+      visible: true,
+      dataType: 'string',
+    }
+
+    component.currentColumn = currentColumn
+
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('h1.header-title'));
+
+    expect(element.nativeElement.textContent).toEqual('Test Caption');
+  });
+
+  it('should display a cross icon in the header by default', () => {
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('button.actions-header-modal mat-icon'));
+
+    expect(element.nativeElement.textContent).toEqual('clear');
+  });
+
+  it('should display a left arrow icon in the header when a column is selected', () => {
+    const currentColumn: ColumnDatagridModel = {
+      caption: 'Test Caption',
+      dataField: 'testdataField',
+      visible: true,
+      dataType: 'string',
+    }
+
+    component.currentColumn = currentColumn
+
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('button.actions-header-modal mat-icon'));
+
+    expect(element.nativeElement.textContent).toEqual('keyboard_arrow_left');
+  });
+
+  it('should unselect the column correctly', () => {
+    const currentColumn: ColumnDatagridModel = {
+      caption: 'Test Caption',
+      dataField: 'testdataField',
+      visible: true,
+      dataType: 'string',
+    }
+
+    component.currentColumn = currentColumn
+
+    fixture.detectChanges();
+
+    const element: DebugElement = fixture.debugElement.query(By.css('button.actions-header-modal mat-icon'));
+
+    expect(element.nativeElement.textContent).toEqual('keyboard_arrow_left');
+
+    const button: DebugElement = fixture.debugElement.query(By.css('button.actions-header-modal'));
+
+    button.triggerEventHandler('click', null);
+
+    expect(component.currentColumn).toBeNull()
+
+    // l'icon devrait être clear...
+    // expect(element.nativeElement.textContent).toEqual('clear');
+  });
+
+  it('should display columns in the list correctly', () => {
+    fixture.detectChanges();
+
+    const columns: DebugElement[] = fixture.debugElement.queryAll(By.css('cocoring-datagrid-filter-column'));
+
+    expect(columns.length).toEqual(2);
+  })
 });
