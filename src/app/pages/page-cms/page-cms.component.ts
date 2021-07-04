13c36ea2e8bf8f45f6b21bf2b442c126db939fa7
nicos <nico.sesma@gmail.com>
2021-07-04 23:08:18 +0200

    cms affichage cms page en lecture seule

--- src/app/pages/page-cms/page-cms.component.ts
+++ src/app/pages/page-cms/page-cms.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { CocoringCmsComponent, SectionPageDatasModel } from '@cocori-ng/lib/src/lib/feature-cms';
-import { ConfigCmsModel, SectionModel } from '@cocori-ng/lib/src/lib/feature-cms/core/model/cms.model';
+import { ConfigCmsModel } from '@cocori-ng/lib/src/lib/feature-cms/core/model/cms.model';
 import { StorageService } from '@cocori-ng/lib/src/lib/feature-core';
 import { EnvironmentService } from 'src/app/core/service/environment.service';
 
@@ -23,36 +23,12 @@ export class PageCMSComponent implements OnInit {
   ngOnInit() {
     this.initConfigCms()
 
-    /** fonction de transfert */
+    /** on affiche à nouveau la config cms de la apge sauvegardée en localstorage */
+    this.datasCms = this.getDatasFromLocalstorage() || []
+  }
 
-    const datasToTransform: SectionPageDatasModel[] = [
-      {
-        "id":"034d03f4-1016-0d03-bf9e-0f5eb6217d7b",
-        "backgroundColor": "#343a40",
-        "template": "CenterZoneTpl",
-        "values": {
-          "editor1": "<h1 style=\"text-align: center;\"><span style=\"color: #ffffff;\">Made with ❤️ by Cocorisoft</span></h1>"
-        }
-      }]
-
-    const transformInto: SectionModel = {
-      "idSection": "034d03f4-1016-0d03-bf9e-0f5eb6217d7b",
-      "block": {
-        "component": "CenterZoneTpl",
-        "data": {
-          "idBlock": "987CE6B5-F5F3-40BC-8760-59D52811DBD9",
-          "filename": "1.jpg",
-          "label": "bloc avec une zone de texte",
-          "backgroundColor": "#343a40",
-          "content": {
-            "editor1": "<h1 style=\"text-align: center;\"><span style=\"color: #ffffff;\">Made with ❤️ by Cocorisoft</span></h1>"
-          }
-        }
-      },
-      "backgroundColor": "#343a40", "values": null
-    }
-
-    this.datasCms = datasToTransform
+  public getDatasFromLocalstorage(): SectionPageDatasModel[] {
+    return this.storageService.getLocalStorageItem('cms-page-save')
   }
 
   public onSaveBtn(datas: SectionPageDatasModel[]) {
