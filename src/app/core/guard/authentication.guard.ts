00dc49117bc11efd1d37d353237dfb12f8123239
nicos <nico.sesma@gmail.com>
2021-09-26 22:55:08 +0200

    sidenav...

---
+++ src/app/core/guard/authentication.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { TokenService } from '@cocori-ng/lib';
+import { Observable } from 'rxjs';
+
+import { LoginApiService } from '../api/LoginApi.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthenticationGuard implements CanActivate {
+    constructor(
+        private tokenService: TokenService,
+        private router: Router, private loginApiService: LoginApiService
+    ) { }
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot,
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        if (this.tokenService.accessToken) {
+            return true;
+        } else {
+            this.loginApiService.redirectUrl = state.url;
+
+            this.router.navigate(['connexion']);
+
+            return false;
+        }
+    }
+}
